23f38f7b62e7daee15c4a09a7d2f4e4b12927849
C22319896 Calvin Delporte <c22319896@mytudublin.ie>
2025-01-31 09:39:28 +0000

    byte sized encoding

--- generate.py
+++ generate.py
@@ -43,20 +43,18 @@ def char_to_binary(char):
     return char_in_binary
 
 
-def image_generation(file_path,binary_input,image_size):
-    # The x and y size of the image in pixels
-    image_size = 38
-    
+def image_generation(file_path,binary_input,image_size):   
+    image_s = int(image_size) 
     # Creates image
-    image = Image.new('RGBA', (image_size, image_size), color='white')
+    image = Image.new('RGBA', (int(image_s), int(image_s)), color='white')
 
     # Gets pixel array of image
     pixels = image.load()
     
-    for y in range(image_size):
-        for x in range(image_size):
-            two_bit_number_set = (y * image_size + x) * 2
-            colour = two_bit_colour(binary_input[two_bit_number_set:two_bit_number_set+2])
+    for y in range(image_s):
+        for x in range(image_s):
+            two_bit_number_set = (y * image_s + x) * 2
+            colour = four_bit_colour(binary_input[two_bit_number_set:two_bit_number_set+4])
             
             if type(colour) == tuple:
                 pixels[x,y] = colour
@@ -83,3 +81,57 @@ def two_bit_colour(two_bit_input):
         case "11":
             return blue
         
+def four_bit_colour(four_bit_input):
+    black = (0,0,0,255)
+    red = (255,0,0,255)
+    yellow = (255,255,0,255)
+    green = (0,255,0,255)
+    cyan = (0,255,255,255)
+    blue = (0,0,255,255)
+    magenta = (255,0,255,255)
+    white = (255,255,255,255)
+    darkred = (128,0,0,255)
+    darkyellow = (128,128,0,255)
+    darkgreen = (0,128,0,255)
+    darkcyan = (0,128,128,255)
+    darkblue = (0,0,128,255)
+    darkmagenta = (128,0,128,255)
+    darkwhite = (171,171,171,255)
+    darkerwhite = (85,85,85,255)
+    
+    if len(four_bit_input) == 4:
+        print(four_bit_input)
+    
+    match four_bit_input:
+        case "0000":
+            return black
+        case "0001":
+            return red
+        case "0010":
+            return yellow
+        case "0011":
+            return green
+        case "0100":
+            return cyan
+        case "0101":
+            return blue
+        case "0110":
+            return magenta
+        case "0111":
+            return white
+        case "1000":
+            return darkred
+        case "1001":
+            return darkyellow
+        case "1010":
+            return darkgreen
+        case "1011":
+            return darkcyan
+        case "1100":
+            return darkblue
+        case "1101":
+            return darkmagenta
+        case "1110":
+            return darkwhite
+        case "1111":
+            return darkerwhite
